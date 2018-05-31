17c0a5dfa95a7431a90b7a702f4dadead2395cef
duy huynh <duy@autonomous.nyc>
2018-05-31 12:40:47 +0700

    rename methods

--- contracts/PredictionHandshake.sol
+++ contracts/PredictionHandshake.sol
@@ -4,7 +4,7 @@
 * but instead matchedes backers/takers (those betting on odds) with layers/makers 
 * (those offering the odds).
 *
-* Conventions:
+* Note:
 *
 *       side: 0 (unknown), 1 (support), 2 (against)
 *       role: 0 (unknown), 1 (maker), 2 (taker)
@@ -38,41 +38,53 @@ contract PredictionHandshake {
                 root = msg.sender;
         } 
 
-        event __init(uint hid, bytes32 offchain); 
+        event __createMarket(uint hid, bytes32 offchain); 
 
-        function init(uint fee, uint closingTime, uint reportTime, bytes32 offchain) public payable {
+        function createMarket(uint fee, uint closingTime, uint reportTime, bytes32 offchain) public payable {
                 Market memory m;
                 m.creator = msg.sender;
                 m.fee = fee;
                 m.closingTime = now + closingTime * 1 seconds;
                 m.reportTime = m.closingTime + reportTime * 1 seconds;
                 markets.push(m);
-                __init(markets.length - 1, offchain);
+                __createMarket(markets.length - 1, offchain);
         }
 
-        event __shake(uint hid, bytes32 offchain);
+        event __init(uint hid, bytes32 offchain);
+        event __debug__init(uint hid, uint stake, uint payout, bytes32 offchain);
 
-        event __debug__shakeByMaker(uint hid, uint stake, uint payout, bytes32 offchain);
-
-        function shakeByMaker(uint hid, uint side, uint payout, bytes32 offchain) public payable {
+        function init(uint hid, uint side, uint payout, bytes32 offchain) public payable {
                 Market storage m = markets[hid];
                 require(now < m.closingTime);
                 m.open[msg.sender][side].stake += msg.value;
                 m.open[msg.sender][side].payout += payout;
-                __shake(hid, offchain);
-                __debug__shakeByMaker(hid, m.open[msg.sender][side].stake, m.open[msg.sender][side].payout, offchain);
+                __init(hid, offchain);
+                __debug__init(hid, m.open[msg.sender][side].stake, m.open[msg.sender][side].payout, offchain);
         }
 
-        event __debug__shakeByTaker__taker(uint hid, uint stake, uint payout, bytes32 offchain);
-        event __debug__shakeByTaker__maker(uint hid, uint matched_stake, uint matched_payout, 
+        event __uninit(uint hid, bytes32 offchain);
+
+        function uninit(uint hid, uint side, uint stake, uint payout, bytes32 offchain) public onlyPredictor(hid) {
+                Market storage m = markets[hid];
+                require(m.open[msg.sender][side].stake >= stake);
+                require(m.open[msg.sender][side].payout >= payout);
+                m.open[msg.sender][side].stake -= stake;
+                m.open[msg.sender][side].payout -= payout;
+                msg.sender.transfer(stake);
+                __uninit(hid, offchain);
+        }
+
+        event __shake(uint hid, bytes32 offchain);
+        event __debug__shake__taker(uint hid, uint stake, uint payout, bytes32 offchain);
+        event __debug__shake__maker(uint hid, uint matched_stake, uint matched_payout, 
                                            uint open_stake, uint open_payout, bytes32 offchain);
 
-        function shakeByTaker(uint hid, uint side, uint payout, address maker, bytes32 offchain) public payable {
+        function shake(uint hid, uint side, uint payout, address maker, bytes32 offchain) public payable {
                 require(maker != 0);
                 Market storage m = markets[hid];
                 require(now < m.closingTime);
 
-                // move maker's order from open (could be partial)
+                // remove maker's order from open (could be partial)
                 m.open[maker][3-side].stake -= (payout - msg.value);
                 m.open[maker][3-side].payout -= payout;
                 require(m.open[maker][3-side].stake >= 0);
@@ -86,29 +98,17 @@ contract PredictionHandshake {
 
                 __shake(hid, offchain);
 
-                __debug__shakeByTaker__taker(hid, m.matched[msg.sender][side].stake, 
+                __debug__shake__taker(hid, m.matched[msg.sender][side].stake, 
                                              m.matched[msg.sender][side].payout, offchain);
-                __debug__shakeByTaker__maker(hid, m.matched[maker][3-side].stake, m.matched[maker][3-side].payout, 
+                __debug__shake__maker(hid, m.matched[maker][3-side].stake, m.matched[maker][3-side].payout, 
                                              m.open[maker][3-side].stake, m.open[maker][3-side].payout, offchain);
         }
 
-        event __unshake(uint hid, bytes32 offchain);
 
-        function unshake(uint hid, uint side, uint stake, uint payout, bytes32 offchain) public onlyPredictor(hid) {
-                Market storage m = markets[hid];
-                require(m.open[msg.sender][side].stake >= stake);
-                require(m.open[msg.sender][side].payout >= payout);
-                m.open[msg.sender][side].stake -= stake;
-                m.open[msg.sender][side].payout -= payout;
-                msg.sender.transfer(stake);
-                __unshake(hid, offchain);
-        }
+        event __collect(uint hid, bytes32 offchain);
 
-
-        event __withdraw(uint hid, bytes32 offchain);
-
-        // as winner
-        function withdraw(uint hid, bytes32 offchain) public onlyPredictor(hid) {
+        // collect winning payout
+        function collect(uint hid, bytes32 offchain) public onlyPredictor(hid) {
                 Market storage m = markets[hid]; 
                 require(m.outcome != 0);
                 require(now > m.closingTime);
@@ -127,7 +127,7 @@ contract PredictionHandshake {
                 msg.sender.transfer(amt);
                 root.transfer(com);
 
-                __withdraw(hid, offchain);
+                __collect(hid, offchain);
         }
 
 
--- test/prediction.js
+++ test/prediction.js
@@ -27,7 +27,6 @@ contract("PredictionHandshake", (accounts) => {
         const taker2 = accounts[7]
         const taker3 = accounts[8]
 
-        const DEADLINE = 3
         const SUPPORT = 1, AGAINST = 2
         const OFFCHAIN = 1
 
@@ -40,14 +39,33 @@ contract("PredictionHandshake", (accounts) => {
         describe('create two test prediction markets', () => {
 
                 it('should create the first prediction market', async () => {
-                        const tx = await hs.init(DEADLINE, OFFCHAIN, { from: creator1 })
-                        eq(0, await oc(tx, "__init", "hid"))
+                        const i = {
+                                fee: 1,
+                                closingTime: 7,
+                                reportTime: 1,
+                                creator: creator1 
+                        }
+                        const o = {
+                                hid: 0
+                        }
+                        const tx = await hs.createMarket(i.fee, i.closingTime, i.reportTime, OFFCHAIN, { from: i.creator})
+                        eq(o.hid, await oc(tx, "__createMarket", "hid"))
                 })
 
                 it('should create the second prediction market', async () => {
-                        const tx = await hs.init(DEADLINE, OFFCHAIN, { from: creator2 })
-                        eq(1, await oc(tx, "__init", "hid"))
+                        const i = {
+                                fee: 2,
+                                closingTime: 3,
+                                reportTime: 2,
+                                creator: creator2 
+                        }
+                        const o = {
+                                hid: 1
+                        }
+                        const tx = await hs.createMarket(i.fee, i.closingTime, i.reportTime, OFFCHAIN, { from: i.creator})
+                        eq(o.hid, await oc(tx, "__createMarket", "hid"))
                 })
+
         })
 
         describe('place make orders', () => {
@@ -65,9 +83,9 @@ contract("PredictionHandshake", (accounts) => {
                                 stake: i.stake,
                                 payout: i.payout
                         }
-                        const tx = await hs.shakeByMaker(i.hid, i.side, i.payout, OFFCHAIN, {from: i.sender, value: i.stake})
-                        eq(o.stake, await oc(tx, "__debug__shakeByMaker", "stake"))
-                        eq(o.payout, await oc(tx, "__debug__shakeByMaker", "payout"))
+                        const tx = await hs.init(i.hid, i.side, i.payout, OFFCHAIN, {from: i.sender, value: i.stake})
+                        eq(o.stake, await oc(tx, "__debug__init", "stake"))
+                        eq(o.payout, await oc(tx, "__debug__init", "payout"))
                 })
 
                 it("should place 2nd make order", async () => {
@@ -83,9 +101,9 @@ contract("PredictionHandshake", (accounts) => {
                                 stake: i.stake * 2,
                                 payout: i.payout * 2
                         }
-                        const tx = await hs.shakeByMaker(i.hid, i.side, i.payout, OFFCHAIN, {from: i.sender, value: i.stake})
-                        eq(o.stake, await oc(tx, "__debug__shakeByMaker", "stake"))
-                        eq(o.payout, await oc(tx, "__debug__shakeByMaker", "payout"))
+                        const tx = await hs.init(i.hid, i.side, i.payout, OFFCHAIN, {from: i.sender, value: i.stake})
+                        eq(o.stake, await oc(tx, "__debug__init", "stake"))
+                        eq(o.payout, await oc(tx, "__debug__init", "payout"))
                 })
         })
 
@@ -106,16 +124,16 @@ contract("PredictionHandshake", (accounts) => {
                                 maker_stake: web3.toWei(0.2),
                                 maker_payout: i.payout
                         }
-                        const tx = await hs.shakeByTaker(i.hid, i.side, i.payout, i.maker, OFFCHAIN, {from: i.sender, value: i.stake})
+                        const tx = await hs.shake(i.hid, i.side, i.payout, i.maker, OFFCHAIN, {from: i.sender, value: i.stake})
 
-                        eq(o.taker_stake, await oc(tx, "__debug__shakeByTaker__taker", "stake"))
-                        eq(o.taker_payout, await oc(tx, "__debug__shakeByTaker__taker", "payout"))
+                        eq(o.taker_stake, await oc(tx, "__debug__shake__taker", "stake"))
+                        eq(o.taker_payout, await oc(tx, "__debug__shake__taker", "payout"))
 
-                        eq(o.maker_stake, await oc(tx, "__debug__shakeByTaker__maker", "matched_stake"))
-                        eq(o.maker_payout, await oc(tx, "__debug__shakeByTaker__maker", "matched_payout"))
+                        eq(o.maker_stake, await oc(tx, "__debug__shake__maker", "matched_stake"))
+                        eq(o.maker_payout, await oc(tx, "__debug__shake__maker", "matched_payout"))
 
-                        eq(0, await oc(tx, "__debug__shakeByTaker__maker", "open_stake"))
-                        eq(o.maker_payout, await oc(tx, "__debug__shakeByTaker__maker", "open_payout"))
+                        eq(0, await oc(tx, "__debug__shake__maker", "open_stake"))
+                        eq(o.maker_payout, await oc(tx, "__debug__shake__maker", "open_payout"))
                 })
 
 
