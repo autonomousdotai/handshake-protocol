a4fd26fc2632bb8b4bcbe9aa8b57020b72bd63cc
duy huynh <duy@autonomous.nyc>
2018-06-13 14:10:49 +0700

    temp

--- contracts/Shuriken.sol
+++ contracts/Shuriken.sol
@@ -4,12 +4,10 @@ import 'openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
 
 contract Shuriken is StandardToken {
 
-        string public symbol = "SRK";
-        string public name = "Shuriken";
         uint public INITIAL_SUPPLY = 27000000;
 
         constructor() public {
                 totalSupply_ = INITIAL_SUPPLY;
-                balances[msg.sender] = INITIAL_SUPPLY;
+                balances[msg.sender] = totalSupply_;
         }
 }
