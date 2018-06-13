aa54a0c69cc53a7eae94c94600fabaacf9622854
duy huynh <duy@autonomous.nyc>
2018-06-13 20:22:55 +0700

    prediction1 test

---
+++ test/prediction1.js
@@ -0,0 +1,21 @@
+// const p2p = artifacts.require("PredictionHandshake")
+
+const u = require('./util.js')
+
+contract("PredictionHandshake", (accounts) => {
+
+        // let hs;
+
+        // before(async () => {
+        //         hs = await p2p.deployed();
+        // })
+
+        describe('unit test', () => {
+
+                it("unit test", async () => {
+
+                        const c = ca("PredictionHandshake", "0xf791e4b0758c5d0b89ce7a388b36d3052a330b2b")
+                })
+
+        })
+})
